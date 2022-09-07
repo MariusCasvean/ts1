15fcb86edb75ff7095026dbaeb204e15fb9f2c8a
MariusCasvean <marius.casvean@yahoo.com>
2022-09-07 14:06:52 +0300

    docs: adds a TODO

--- src/components/Rating.vue
+++ src/components/Rating.vue
@@ -17,6 +17,7 @@ export default defineComponent({
     props: {
         rating: {
             required: true,
+            // TODO: check for this type syntax, to be correct
             type: Number as PropType<number>,
         },
     },
